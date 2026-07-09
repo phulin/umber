c2609e27501e510bad030b8cfcf7295f2d074e51
Patrick Hulin <patrick.hulin@gmail.com>
2026-07-09 10:09:54 -0400

    Remove release clippy from check script

--- scripts/check.sh
+++ scripts/check.sh
@@ -9,7 +9,6 @@ cd "$repo_root"
 
 cargo fmt --all --check
 cargo clippy --workspace --all-targets -- -D warnings
-cargo clippy --workspace --all-targets --release -- -D warnings
 cargo test -p tex-state --features testing replay
 cargo test -p tex-state --features shadow --tests
 cargo test --workspace --tests
